75743a6ee683a01632d90897dc8d818dee0cea72
sharathnair9999 <sharathnair9999@gmail.com>
2021-09-28 19:44:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var initialValue = document.querySelector('#initial-price')
+var quantity = document.querySelector('#quantity')
+var currentValue = document.querySelector('#current-price')
+var submitBtn = document.querySelector('#submit-btn')
+var outputBox = document.querySelector('#output-box')
+submitBtn.addEventListener("click", submitValues)
+
+function validateFields(){
+  if(initialValue ===""||quantity===""||currentValue===""){
+    alert("input all please!")
+  }
+  return;
+}
+
+function submitValues(){
+  var ip = Number(initialValue.value)
+  var qty = Number(quantity.value)
+  var curr = Number(currentValue.value)
+  calculateProfitAndLoss(ip,qty,curr)
+}
+
+function outputMessage(message,color){
+  outputBox.innerHTML = message
+  outputBox.style.backgroundColor=color
+    outputBox.style.color='white'
+    outputBox.style.width='300px'
+    outputBox.style.marginRight='auto'
+    outputBox.style.marginTop='1rem'
+    outputBox.style.marginLeft='auto'
+    outputBox.style.padding='1rem 2rem'
+    outputBox.style.borderRadius='1rem'
+}
+
+function calculateProfitAndLoss(initial, quantity, current){
+  if(initial>current){
+    var loss = (initial-current)*quantity
+    var lossPercent = (loss/(initial*quantity))*100;
+   message = `The loss is ${loss} and loss percentage is ${lossPercent}%`
+   outputMessage(message, '#EF4444')
+    
+    
+  
+  }
+  else if(current>initial){
+    var profit = (current-initial)*quantity
+    var profitPercent = (profit/(initial*quantity))*100;
+    message =`The profit is ${profit} and profit percentage is ${profitPercent}%`
+    outputMessage(message, 'green')
+  }
+  else{
+    message ='No pain no Gain!'
+    outputMessage(message,'#60A5FA')
+  }
+}
+
+
+
